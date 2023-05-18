134950a383ef285e58beb82aa02e9179250613d4
Mahadi Hasan <mahadihasan8461@gmail.com>
2023-05-18 11:41:38 +0600

    singup page design done

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,21 @@
 import Lottie from "lottie-react";
+import { useState } from "react";
+import { FcGoogle } from "react-icons/fc";
+import { Link } from "react-router-dom";
 import car from "../../assets/animation/car-safety.json";
 
 const Login = () => {
+  const [error, setError] = useState("");
+  const handleLogin = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(email, password);
+
+    form.reset();
+  };
+
   return (
     <div>
       <div className="hero min-h-screen ">
@@ -11,34 +25,57 @@ const Login = () => {
           </div>
           <div className="card flex-shrink-0 w-full max-w-sm ">
             <div className="card-body">
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text text-xl">Email</span>
-                </label>
-                <input
-                  type="email"
-                  placeholder="email"
-                  className="input input-bordered input-accent"
-                />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text text-xl">Password</span>
-                </label>
-                <input
-                  type="text"
-                  placeholder="password"
-                  className="input input-bordered input-accent"
-                />
-                <label className="label">
-                  <a href="#" className="label-text-alt link link-hover">
-                    Forgot password?
-                  </a>
-                </label>
-              </div>
-              <div className="form-control mt-6">
-                <button className="btn btn-primary bg-[#05058a]">Login</button>
-              </div>
+              <form onSubmit={handleLogin}>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text text-xl">Email</span>
+                  </label>
+                  <input
+                    type="email"
+                    placeholder="email"
+                    name="email"
+                    className="input input-bordered input-accent"
+                    required
+                  />
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text text-xl">Password</span>
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    placeholder="password"
+                    className="input input-bordered input-accent"
+                    required
+                  />
+                  <label className="label">
+                    <a href="#" className="label-text-alt link link-hover">
+                      Forgot password?
+                    </a>
+                  </label>
+                </div>
+                {error && <span className="text-red-600">{error}</span>}
+                <div className="form-control mt-3">
+                  <input
+                    type="submit"
+                    value="Login"
+                    className="btn btn-primary bg-[#05058a]"
+                  />
+                </div>
+              </form>
+            </div>
+            <div className="text-center">
+              <h5>
+                Do not Have an account?{" "}
+                <Link to="/register" className="underline">
+                  Register Here!
+                </Link>
+              </h5>
+              <p className="mt-3 font-medium">Login With</p>
+              <button className="text-3xl mt-4">
+                <FcGoogle />
+              </button>
             </div>
           </div>
         </div>
--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,5 +1,110 @@
+import Lottie from "lottie-react";
+import { useState } from "react";
+import { FcGoogle } from "react-icons/fc";
+import { Link } from "react-router-dom";
+import car from "../../assets/animation/car-in-movement.json";
+
 const Signup = () => {
-  return <div></div>;
+  const [error, setError] = useState("");
+  const handleSingUp = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(email, password);
+
+    form.reset();
+  };
+  return (
+    <div>
+      <div className="hero min-h-screen ">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="text-center lg:text-left">
+            <Lottie animationData={car} loop={true} height={400} width={400} />
+          </div>
+          <div className="card flex-shrink-0 w-full max-w-sm ">
+            <div className="card-body">
+              <form onSubmit={handleSingUp}>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text text-xl">Name</span>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="Name"
+                    name="name"
+                    className="input input-bordered input-accent"
+                    required
+                  />
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text text-xl">Photo Url</span>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="Photo Url"
+                    name="photo"
+                    className="input input-bordered input-accent"
+                    required
+                  />
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text text-xl">Email</span>
+                  </label>
+                  <input
+                    type="email"
+                    placeholder="email"
+                    name="email"
+                    className="input input-bordered input-accent"
+                    required
+                  />
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text text-xl">Password</span>
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    placeholder="password"
+                    className="input input-bordered input-accent"
+                    required
+                  />
+                  <label className="label">
+                    <a href="#" className="label-text-alt link link-hover">
+                      Forgot password?
+                    </a>
+                  </label>
+                </div>
+                {error && <span className="text-red-600">{error}</span>}
+                <div className="form-control mt-3">
+                  <input
+                    type="submit"
+                    value="Login"
+                    className="btn btn-primary bg-[#05058a]"
+                  />
+                </div>
+              </form>
+            </div>
+            <div className="text-center">
+              <h5>
+                Already have an account?
+                <Link to="/register" className="underline">
+                  Login Now!
+                </Link>
+              </h5>
+              <p className="mt-3 font-medium">SignUp With</p>
+              <button className="text-3xl mt-4">
+                <FcGoogle />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
