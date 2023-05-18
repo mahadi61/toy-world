c835eb6bdf7fb9f549fa6dfcef9a7314ca054b1e
Mahadi Hasan <mahadihasan8461@gmail.com>
2023-05-19 01:12:46 +0600

    all toy added

--- src/pages/allToys/AllToys.jsx
+++ src/pages/allToys/AllToys.jsx
@@ -1,5 +1,36 @@
 const AllToys = () => {
-  return <div>This is all toys</div>;
+  return (
+    <div>
+      <div className="overflow-x-auto w-11/12 mx-auto my-4">
+        <table className="table w-full">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>Seller</th>
+              <th>Toy Name</th>
+              <th>Sub-category</th>
+              <th>Price</th>
+              <th>Available Quantity</th>
+              <th>Details</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <th>
+                <button className="btn  btn-success">details</button>
+              </th>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default AllToys;
